1aa6a05ad348372f495b76532720189c178fd826
Vitor <direcaoass@gmail.com>
2023-01-08 15:03:28 -0300

    left menu added

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const container = document.querySelector('.container');
-const button = document.querySelector('#new-size');
-
+const container = document.querySelector('.grid-container');
+const sizeButton = document.querySelector('#new-size');
+const rainbowOption = document.querySelector('#rainbow-color');
+const unicOption = document.querySelector('#unic-color');
+const colorPicked = document.querySelector('#pick-color');
+const clearButton = document.querySelector('#clear')
+let penStatus = false;
 
 function createSquares(squaresQty) {
     for (let i = 1; i <= (squaresQty ** 2); i++) {
@@ -12,53 +16,47 @@ function createSquares(squaresQty) {
 
 
 function changeSize() {
-    const newSize = button.value;
+    const newSize = sizeButton.value;
     container.innerText = '';
     container.style.setProperty('grid-template-columns', `repeat(${newSize}, 1fr)`);
     container.style.setProperty('grid-template-rows', `repeat(${newSize}, 1fr)`);
     createSquares(newSize);
+    console.log(newSize);
 
 }
 
-function createRGB(factor) {
+function createRandonRGB(factor) {
     const randColor1 = factor * Math.floor(Math.random() * 255);
     const randColor2 = factor * Math.floor(Math.random() * 255);
     const randColor3 = factor * Math.floor(Math.random() * 255);
     return `${randColor1},${randColor2},${randColor3}`
 }
 
+function startPaint() {
+
+    container.addEventListener('mouseover', (e) => {
+        const item = e.target;
+        if (rainbowOption.checked && item.classList.contains('square') && penStatus)
+            item.style.setProperty('background-color', `rgb(${(createRandonRGB(1))})`);
+        else if (unicOption.checked && item.classList.contains('square') && penStatus)
+            item.style.backgroundColor = `${colorPicked.value}`;
+    })
+}
+
+
 //EVENTS
+createSquares(36);
 
+container.addEventListener('click', () => {
+    penStatus = !penStatus;
+    startPaint();
+})
 
-createSquares(16);
-
-button.addEventListener('mousemove', () => {
+sizeButton.addEventListener('change', () => {
     changeSize()
 })
 
 
-container.addEventListener('mouseover', (e) => {
-
-    const item = e.target;
-
-    if (item.classList.contains('square') && !item.dataset.darken) {
-
-        item.style.setProperty('background-color', `rgb(${(createRGB(1))})`);
-        item.dataset.darken = 5;
-    }
-
-    else if (item.classList.contains('square') && item.dataset.darken == 5) {
-
-        item.style.setProperty('background-color', `rgb(${(createRGB(0.5))})`);
-        item.dataset.darken = 10;
-    }
-
-    else if (item.classList.contains('square') && item.dataset.darken == 10) {
-
-        item.style.setProperty('background-color', `rgb(${(createRGB(0))})`);
-
-    }
+clearButton.addEventListener('click', () => {
+    changeSize();
 })
-
-
-
